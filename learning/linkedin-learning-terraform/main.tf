a947677152e52b58c2b5b3d0439bdf908ba8b2ca
plozovik <plozovikov@gmail.com>
2024-10-26 15:53:35 +0200

    extended security_group_external_module

--- learning/linkedin-learning-terraform/main.tf
+++ learning/linkedin-learning-terraform/main.tf
@@ -17,9 +17,9 @@ module "vpc_external_module" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.14.0"
 
-  azs             = ["eu-central-1", "eu-north-1", "eu-west-1"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  cidr           = "10.0.0.0/16"
+  azs            = ["eu-central-1", "eu-north-1", c]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
@@ -33,10 +33,11 @@ module "security_group_external_module" {
   version = "5.2.0"
   name    = "test-learning-sg"
 
-  vpc_id = module.vpc_external_module.vpc_id
-
-  egress_rules  = ["all-all"]
-  ingress_rules = ["https-443-tcp", "http-80-tcp"]
+  vpc_id              = module.vpc_external_module.vpc_id
+  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 
   tags = {
     Terraform   = "true"
@@ -49,12 +50,13 @@ module "ec2_instance_external_module" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.7.1"
 
-  name                   = "test-instance"
+  instance_type = "t2.micro"
+  ami           = "ami-08ec94f928cf25a9d"
+  name          = "test-instance"
+
   subnet_id              = module.vpc_external_module.public_subnets[0]
   vpc_security_group_ids = [module.security_group_external_module.security_group_id]
 
-  instance_type = "t2.micro"
-  ami           = "ami-08ec94f928cf25a9d"
 
   tags = {
     Terraform   = "true"
