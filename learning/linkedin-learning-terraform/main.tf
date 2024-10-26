1c3a30b3387c8b3c109c7c2afb4a6b689faa4bfc
plozovik <plozovikov@gmail.com>
2024-10-26 16:03:18 +0200

    fixed availability zones names

--- learning/linkedin-learning-terraform/main.tf
+++ learning/linkedin-learning-terraform/main.tf
@@ -18,7 +18,7 @@ module "vpc_external_module" {
   version = "5.14.0"
 
   cidr           = "10.0.0.0/16"
-  azs            = ["eu-central-1", "eu-north-1", "eu-west-1"]
+  azs            = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
