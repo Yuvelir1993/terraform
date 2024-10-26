55d4989fafa4218d8fb6cad5854748b5d53a4a5a
plozovik <plozovikov@gmail.com>
2024-10-26 15:44:52 +0200

    added "eu-west-1"

--- learning/linkedin-learning-terraform/main.tf
+++ learning/linkedin-learning-terraform/main.tf
@@ -17,7 +17,7 @@ module "vpc_external_module" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.14.0"
 
-  azs             = ["eu-central-1", "eu-north-1"]
+  azs             = ["eu-central-1", "eu-north-1", "eu-west-1"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
